cbc4146e484f03533fcf2f791fee9ecd2eaf2ade
simoncardenas52903 <simon.cardenas52903@ucaldas.edu.co>
2026-02-23 17:53:46 -0500

    Update ArbolAVL.py

--- Clases/ArbolAVL.py
+++ Clases/ArbolAVL.py
@@ -1,3 +1,21 @@
+from Clases.NodoAVL import Nodo
+
 class ArbolAVL:
     def __init__(self):
-        pass
+        self.raiz = None
+
+    def insertar(self, valor):
+        self.raiz = self.__insertar(self.raiz, valor)
+
+    def __insertar(self, nodo, valor):
+        if nodo is None:
+            nodo = Nodo(valor)
+            return nodo
+
+        if nodo.valor < valor:
+            nodo.hijoIzquierdo = self.__insertar(nodo.hijoIzquierdo, valor)
+        elif nodo.valor > valor:
+            nodo.hijoDerecho = self.__insertar(nodo.hijoDerecho, valor)
+        else:
+            return nodo
+        return nodo
