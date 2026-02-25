db5a1c3bd01d499f7fbc1165f40348941199aa4a
simoncardenas52903 <simon.cardenas52903@ucaldas.edu.co>
2026-02-24 20:53:42 -0500

    Update Nodo.py

--- Clases/NodoAVL.py
+++ Clases/NodoAVL.py
@@ -1,7 +1,30 @@
 class Node:
-    def __init__(self, flight):
-        self.flight = flight
+    def __init__(self, value):
+        self.value = value
         self.father = None
         self.leftSon = None
         self.rightSon = None
 
+    def getValue(self):
+        return self.value
+
+    def setValue(self, node):
+        self.value = node
+
+    def getFather(self):
+        return self.father
+
+    def setFather(self, node):
+        self.father = node
+
+    def getLeftSon(self):
+        return self.leftSon
+
+    def setLeftSon(self, node):
+        self.leftSon = node
+
+    def getRightSon(self):
+        return self.rightSon
+
+    def setRightSon(self, node):
+        self.rightSon = node
