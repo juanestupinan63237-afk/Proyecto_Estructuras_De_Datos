db2fdb60f9856d1d5bd61644487876a0093a0f9c
simoncardenas52903 <simon.cardenas52903@ucaldas.edu.co>
2026-03-01 22:25:22 -0500

    update SkyBalance.py

--- SkyBalance.py
+++ SkyBalance.py
@@ -1,16 +1,14 @@
 from Classes.BSTTree import BST
-from Classes.AVL import AVLTree
 from Classes.FlightSB import Flight
 from flask import Flask, jsonify, render_template, request, Response
 
 app = Flask(__name__)
 
-bst_tree = BST()
-avl_tree = AVLTree()
+tree = BST()
 
 @app.route("/")
 def home():
-    grafico_svg = avl_tree.RenderTree()
+    grafico_svg = tree.RenderTree()
     return render_template("index.html", grafico=grafico_svg)
 
 @app.route("/Sent/Node", methods=['POST'])
@@ -37,8 +35,7 @@ def RecibirVuelo():
             alert=False
         )
 
-        bst_tree.insertNode(nuevo_vuelo)
-        avl_tree.insertNodeAVL(nuevo_vuelo)
+        tree.insertNode(nuevo_vuelo)
 
         return jsonify({
             "status": "success",
@@ -46,35 +43,12 @@ def RecibirVuelo():
         }), 200
 
     except Exception as e:
-        return jsonify({
-            "status": "error",
-            "message": str(e)
-        }), 400
+        return jsonify({"status": "error", "message": str(e)}), 400
 
-@app.route("/Render/AVL", methods=['GET'])
-def RenderAVLRoute():
-    svg = avl_tree.RenderTree()
+@app.route("/Render/Tree", methods=['GET'])
+def RenderTreeRoute():
+    svg = tree.RenderTree()
     return Response(svg, mimetype='image/svg+xml')
-
-@app.route("/Render/BST", methods=['GET'])
-def RenderBSTRoute():
-    svg = bst_tree.RenderTree()
-    return Response(svg, mimetype='image/svg+xml')
-
-@app.route("/Print", methods=['POST'])
-def PrintPreOrder():
-    result = []
-
-    def preorder(node):
-        if node is None:
-            return
-        result.append(node.getFlightCode())
-        preorder(node.getLeftSon())
-        preorder(node.getRightSon())
-
-    preorder(avl_tree.root)
-
-    return jsonify({"preorder": result})
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
