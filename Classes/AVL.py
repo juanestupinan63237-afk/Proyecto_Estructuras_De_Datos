052d181111df419536d6ca1c1fbd34f8e1917d98
simoncardenas52903 <simon.cardenas52903@ucaldas.edu.co>
2026-03-01 22:33:01 -0500

    update AVL.py

--- Classes/AVL.py
+++ Classes/AVL.py
@@ -1,5 +1,6 @@
 from Classes.NodeAVL import Node
 from Classes.FlightSB import Flight
+
 class AVLTree:
     def __init__(self):
         self.root = None
@@ -7,14 +8,21 @@ class AVLTree:
     def insertNodeAVL(self, value: Flight):
         self.root = self.__insertNodeAVL(self.root, value)
 
-    def __insertNodeAVL(self, node : Node, value: Flight):
+    def __insertNodeAVL(self, node: Node, value: Flight):
         if node is None:
-            node = Node(value)
-            return node
+            return Node(value)
+
         if value.getCode() < node.getFlightCode():
             node.setLeftSon(self.__insertNodeAVL(node.getLeftSon(), value))
         elif value.getCode() > node.getFlightCode():
             node.setRightSon(self.__insertNodeAVL(node.getRightSon(), value))
         else:
-            raise Exception(f"This Node already exists in our AVL Tree...")
+            raise Exception("This Node already exists in our AVL Tree...")
+
+        node.setHeight(1 + max(self.getHeight(node.getLeftSon()), self.getHeight(node.getRightSon())))
         return node
+
+def getHeight(self, node):
+    if node is None:
+        return 0
+    return node.getHeight()
