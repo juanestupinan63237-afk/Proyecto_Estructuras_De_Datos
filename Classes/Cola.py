520359d034227b6de2fb7b8ba0f58e2b0b32e113
isabelladuque-cmyk <isabella.duque@autonoma.edu.co>
2026-03-27 15:09:45 -0500

    cola

--- Classes/Cola.py
+++ Classes/Cola.py
@@ -3,11 +3,11 @@ class Cola:
     def __init__(self):
         self.__datos__ = []
     def Encolar (self, datos):
-        self.queue.append(datos)
+        self.__datos__.append(datos)
     def Desencolar (self):
         if self.isEmpty():
             return "Esta vacío"
-        return self.queue.pop()
+        return self.__datos__.pop()
     def isEmpty(self):
         return len(self.__datos__) == 0
     
