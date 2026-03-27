2d6b39f596b5301e4c07cd9c648f4a0b3796cc1c
juanestupinan63237-afk <juan.estupinan63237@ucaldas.edu.co>
2026-03-27 18:09:39 -0500

    Update Cola.py

--- Classes/Cola.py
+++ Classes/Cola.py
@@ -7,7 +7,7 @@ class Cola:
     def Desencolar (self):
         if self.isEmpty():
             return "Esta vacío"
-        return self.__datos__.pop()
+        return self.__datos__.pop(0)
     def isEmpty(self):
         return len(self.__datos__) == 0
     
